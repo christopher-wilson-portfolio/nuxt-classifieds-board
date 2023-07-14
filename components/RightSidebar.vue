49d4d18cca8e7b82caad24992792668eb7829d32
Christopher Wilson <cwilson@christopherwilson.biz>
2023-07-14 14:34:53 -0500

    Added new components and layouts

---
+++ components/RightSidebar.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <aside>
+    <section id="language">
+      <select name="languages" id="select-language">
+        <option value="en">English</option>
+      </select>
+    </section>
+
+    <section id="loctations">
+      <header>nearby</header>
+      <ul>
+        <li>location a</li>
+        <li>location b</li>
+      </ul>
+    </section>
+  </aside>
+</template>
+
+<style scoped>
+  aside {
+    background-color: rgb(244, 244, 244);
+  }
+  ul {
+    list-style-type: none;
+  }
+</style>
